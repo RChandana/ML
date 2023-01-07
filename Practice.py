db7d12cf24fd5d8895f983612366e806deebbe93
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-07 10:57:10 +0530

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,3 +1,9 @@
-# Pandas Basic
+# Pandas Basics
 import pandas as pd
 pd.DataFrame({'Yes' : [12, 23], 'No' : [54, 65]}, index = ['Person 1', 'Person 2'])
+
+
+# Series Basics
+import pandas as pd
+list = pd.Series([1, 2, 3, 4], index = ['stu1', 'stu2', 'stu3', 'stu4'])
+print(list)
