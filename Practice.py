728f7307424a5c011811561295026e0e479776c5
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2023-01-07 10:58:44 +0530

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -5,5 +5,5 @@ pd.DataFrame({'Yes' : [12, 23], 'No' : [54, 65]}, index = ['Person 1', 'Person 2
 
 # Series Basics
 import pandas as pd
-list = pd.Series([1, 2, 3, 4], index = ['stu1', 'stu2', 'stu3', 'stu4'])
+list = pd.Series([1, 2, 3, 4], index = ['stu1', 'stu2', 'stu3', 'stu4'], name = 'Student')
 print(list)
